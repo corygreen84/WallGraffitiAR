67d2b258b18b6dbf5015e4c61a1fa630a4648c5a
SugarpineSoftware <corygreen84@hotmail.com>
2020-05-10 16:32:30 -0700

    Start of project

---
+++ WallGraffitiAR/WallGraffitiAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WallGraffitiAR
+//
+//  Created by Cory Green on 5/10/20.
+//  Copyright © 2020 Cory Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
